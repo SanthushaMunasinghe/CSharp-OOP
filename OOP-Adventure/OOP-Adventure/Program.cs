10f3084721bc825e71c33f13d13d58c5b5afb63c
Santhusha Munasinghe <santhushamunasinghe601@gmail.com>
2023-05-31 22:32:03 +0530

    commint 01
    Abstraction, Inheritance, Encapsulation

---
+++ OOP-Adventure/OOP-Adventure/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOPAdventure;
+
+var language = new English();
+
+Console.WriteLine(language.ChooseYourName);
+
+var name = Console.ReadLine();
+
+if (name == string.Empty)
+    name = "Player";
+
+var player = new Player(name);
+
+Console.WriteLine("Hello {0}, to your OOP Adventure!", player.Name);
---
+++ OOP-Adventure/OOP-Adventure/Src/Text/English.cs
@@ -0,0 +1,9 @@
+﻿using OOPAdventure;
+
+public class English : Language
+{
+    public English()
+    {
+        ChooseYourName = "Hello! What is your name?";
+    }
+}
